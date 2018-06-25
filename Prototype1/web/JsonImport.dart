1292301f22bccc33c8ab5086aea8660c5d78136c
MartinDolac <34304794+MartinDolac@users.noreply.github.com>
2018-06-25 19:07:35 +0200

    JsonImport

--- Prototype1/web/JsonImport.dart
+++ Prototype1/web/JsonImport.dart
@@ -2,10 +2,11 @@ import 'dart:convert';
 import 'dart:html';
 
 void jsonImport() {
-  String url = 'test.json';
+  String url = 'Field.json';
 
   print("HALLO");
   HttpRequest.getString(url).then((str){
+    str.split('Level');
     print(JSON.decode(str));
   });
 }
